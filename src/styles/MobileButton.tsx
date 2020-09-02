78751aab2972822965500130ad297a323ad03f82
crispyyv <lamer079@gmail.com>
2020-09-02 13:39:01 +0300

    swap sort by date to float

--- src/styles/MobileButton.tsx
+++ src/styles/MobileButton.tsx
@@ -5,7 +5,7 @@ export const MobileButton = styled.button`
   display: none;
   @media only screen and (max-width: 480px) {
     display: block;
-    position: absolute;
+    position: fixed;
     right: 10px;
     top: 35px;
   }
